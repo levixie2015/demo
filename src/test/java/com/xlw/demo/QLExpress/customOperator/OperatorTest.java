cf5c445cce7b90e0db8d30bf0dbba96614eae003
xieliwei <xieliwei@fertex.cn>
2024-08-09 10:56:57 +0800

    QLExpress单元测试

--- src/test/java/com/xlw/demo/QLExpress/customOperator/OperatorTest.java
+++ src/test/java/com/xlw/demo/QLExpress/customOperator/OperatorTest.java
@@ -126,4 +126,24 @@ public class OperatorTest {
         }
         return result;
     }
+
+    /**
+     * 关于集合的快捷写法
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSet() throws Exception {
+        ExpressRunner runner = new ExpressRunner(false, false);
+        DefaultContext<String, Object> context = new DefaultContext<String, Object>();
+        String express = "abc = NewMap(1:1, 2:2); return abc.get(1) + abc.get(2);";
+        Object r = runner.execute(express, context, null, false, false);
+        System.out.println(r);
+        express = "abc = NewList(1, 2, 3); return abc.get(1) + abc.get(2)";
+        r = runner.execute(express, context, null, false, false);
+        System.out.println(r);
+        express = "abc = [1, 2, 3]; return abc[1] + abc[2];";
+        r = runner.execute(express, context, null, false, false);
+        System.out.println(r);
+    }
 }
