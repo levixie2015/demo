4e259440de17f746ef24240eebf036372559a8bf
xieliwei <xieliwei@fertex.cn>
2024-08-09 10:39:16 +0800

    QLExpress单元测试

--- src/test/java/com/xlw/demo/QLExpress/customOperator/OperatorTest.java
+++ src/test/java/com/xlw/demo/QLExpress/customOperator/OperatorTest.java
@@ -2,6 +2,7 @@ package com.xlw.demo.QLExpress.customOperator;
 
 import com.ql.util.express.DefaultContext;
 import com.ql.util.express.ExpressRunner;
+import com.ql.util.express.IExpressContext;
 
 public class OperatorTest {
 
@@ -53,5 +54,23 @@ public class OperatorTest {
         String express = "取绝对值(-100); 转换为大写(\"hello world\"); 打印(\"你好吗？\"); contains(\"helloworld\",\"aeiou\")";
         Object result = runner.execute(express, context, null, false, false);
         System.out.println(result);
+    }
+
+    /**
+     * macro 宏定义
+     *
+     * @throws Exception
+     */
+    @org.junit.jupiter.api.Test
+    public void macroTest() throws Exception {
+        ExpressRunner runner = new ExpressRunner();
+        runner.addMacro("计算平均成绩", "(语文+数学+英语)/3.0");
+        runner.addMacro("是否优秀", "计算平均成绩>90");
+        IExpressContext<String, Object> context = new DefaultContext<String, Object>();
+        context.put("语文", 88);
+        context.put("数学", 99);
+        context.put("英语", 95);
+        Object result = runner.execute("是否优秀", context, null, false, false);
+        System.out.println(result);//返回结果true
     }
 }
