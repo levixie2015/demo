392cf360d49468ed7a43e3f52bf2fc677fba8291
xieliwei <xieliwei@fertex.cn>
2024-08-09 10:36:53 +0800

    QLExpress单元测试

--- src/test/java/com/xlw/demo/QLExpress/customOperator/OperatorTest.java
+++ src/test/java/com/xlw/demo/QLExpress/customOperator/OperatorTest.java
@@ -34,4 +34,24 @@ public class OperatorTest {
         Object r = runner.execute("join(1, 2, 3)", context, null, false, false);
         System.out.println(r); // 返回结果 [1, 2, 3]
     }
+
+    /**
+     * 绑定java类或者对象的method
+     *
+     * @throws Exception
+     */
+    @org.junit.jupiter.api.Test
+    public void addFunctionOfClass() throws Exception {
+        ExpressRunner runner = new ExpressRunner();
+        DefaultContext<String, Object> context = new DefaultContext<String, Object>();
+        runner.addFunctionOfClassMethod("取绝对值", Math.class.getName(), "abs", new String[]{"double"}, null);
+        runner.addFunctionOfClassMethod("转换为大写", BeanExample.class.getName(), "upper", new String[]{"String"}, null);
+
+        runner.addFunctionOfServiceMethod("打印", System.out, "println", new String[]{"String"}, null);
+        runner.addFunctionOfServiceMethod("contains", new BeanExample(), "anyContains", new Class[]{String.class, String.class}, null);
+
+        String express = "取绝对值(-100); 转换为大写(\"hello world\"); 打印(\"你好吗？\"); contains(\"helloworld\",\"aeiou\")";
+        Object result = runner.execute(express, context, null, false, false);
+        System.out.println(result);
+    }
 }
