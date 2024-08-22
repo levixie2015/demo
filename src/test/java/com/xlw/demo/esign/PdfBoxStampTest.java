33a0e57d7b9ad7fcafd2bafc90dbcbfeccab1ca1
xieliwei <xieliwei@fertex.cn>
2024-08-22 17:43:13 +0800

    PDFBox库来实现PDF文件的盖章

--- src/test/java/com/xlw/demo/esign/PdfBoxStampTest.java
+++ src/test/java/com/xlw/demo/esign/PdfBoxStampTest.java
@@ -18,7 +18,7 @@ public class PdfBoxStampTest {
     public void PdfBoxStampTest() {
         try (PDDocument document = PDDocument.load(new File("/Users/xieliwei/Desktop/电子签章测试/瑞康医药集团河北有限公司.pdf"))) {
             // 加载印章图像
-            PDImageXObject pdImage = PDImageXObject.createFromFile("/Users/xieliwei/Desktop/电子签章测试/测试章.png", document);
+            PDImageXObject pdImage = PDImageXObject.createFromFile("/Users/xieliwei/Desktop/电子签章测试/益通数科章.png", document);
 
             // 获取第一页（索引从0开始）
             PDPage page = document.getPage(0);
@@ -28,13 +28,13 @@ public class PdfBoxStampTest {
                 // 设置印章图像的位置和尺寸
                 // 注意：PDFBox的坐标系统左下角为原点(0,0)，向右为x轴正方向，向上为y轴正方向
                 float x = 100; // 印章的x坐标
-                float y = 600; // 印章的y坐标（根据页面大小调整）
+                float y = 700; // 印章的y坐标（根据页面大小调整）
                 // 假设印章图像不需要缩放
                 float width = pdImage.getWidth();
                 float height = pdImage.getHeight();
 
                 // 将印章图像添加到PDF页面
-                contents.drawImage(pdImage, x, y, width, height);
+                contents.drawImage(pdImage, x, y, width/2, height/2);
             }
 
             // 保存修改后的PDF
