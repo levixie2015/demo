9189781f92d7f2dcb205fa5fb99927a552c4f561
xieliwei <xieliwei@fertex.cn>
2024-08-23 15:46:35 +0800

    PDFBox库来实现PDF文件的关键字盖章

--- src/test/java/com/xlw/demo/esign/PdfBox/PdfBoxStampTest.java
+++ src/test/java/com/xlw/demo/esign/PdfBox/PdfBoxStampTest.java
@@ -26,12 +26,12 @@ public class PdfBoxStampTest {
         float widthScale = 1f;//印章宽度缩放比例
         float heightScale = 1f;//印章高度缩放比例
         boolean compress = true;//参数决定了写入的内容是否应该被压缩。如果设置为true，则PDFBox会尝试压缩内容以减少文件大小；如果设置为false，则内容将以未压缩的形式写入。通常，启用压缩是一个好主意，因为它可以减少生成的PDF文件的大小
-//        stampByKeyWords(pdfPath, stampImgPath, keyWords, xOffset, yOffset, widthScale, heightScale, compress);
+        stampByKeyWords(pdfPath, stampImgPath, keyWords, xOffset, yOffset, widthScale, heightScale, compress, true);
 
         //根据绝对位置图片盖章
         float x = 49.182f - 40f; // 印章的x坐标
         float y = 724.82f - 90f; // 印章的y坐标（根据页面大小调整）
-        stampByAbsolutePosition(pdfPath, stampImgPath, 2, x, y, widthScale, heightScale, compress, true);
+//        stampByAbsolutePosition(pdfPath, stampImgPath, 2, x, y, widthScale, heightScale, compress, true);
     }
 
     /**
@@ -45,8 +45,9 @@ public class PdfBoxStampTest {
      * @param widthScale   印章宽度缩放比例
      * @param heightScale  印章高度缩放比例
      * @param compress     参数决定了写入的内容是否应该被压缩。如果设置为true，则PDFBox会尝试压缩内容以减少文件大小；如果设置为false，则内容将以未压缩的形式写入。通常，启用压缩是一个好主意，因为它可以减少生成的PDF文件的大小
+     * @param perforation  是否盖骑缝章
      */
-    public static void stampByKeyWords(String pdfPath, String stampImgPath, String keyWords, float xOffset, float yOffset, float widthScale, float heightScale, boolean compress) {
+    public static void stampByKeyWords(String pdfPath, String stampImgPath, String keyWords, float xOffset, float yOffset, float widthScale, float heightScale, boolean compress, boolean perforation) {
         try (PDDocument doc = PDDocument.load(new File(pdfPath))) {
             PDImageXObject stampImg = PDImageXObject.createFromFile(stampImgPath, doc);
             PDFTextStripperByKeyWord keyWordPosition = new PDFTextStripperByKeyWord(keyWords, pdfPath);
