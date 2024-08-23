9c0b181fe46b17dc54907643866985c203227dfd
xieliwei <xieliwei@fertex.cn>
2024-08-23 17:54:40 +0800

    PDFBox库来实现PDF文件的关键字盖章

--- src/test/java/com/xlw/demo/esign/PdfBox/PdfBoxStampTest.java
+++ src/test/java/com/xlw/demo/esign/PdfBox/PdfBoxStampTest.java
@@ -18,7 +18,8 @@ public class PdfBoxStampTest {
     public static void main(String[] args) {
         String pdfPath = "/Users/xieliwei/Desktop/电子签章测试/瑞康医药集团河北有限公司.pdf";
         String stampImgPath = "/Users/xieliwei/Desktop/电子签章测试/测试章.png";
-        String outPdfPath = "/Users/xieliwei/Desktop/电子签章测试/瑞康医药集团河北有限公司_签章PdfBox.pdf";
+        //文件所在目录+"PdfBox"+文件后缀名称
+        String outPdfPath = pdfPath.substring(0, pdfPath.lastIndexOf('.')) + "PdfBox" + pdfPath.substring(pdfPath.lastIndexOf('.'));
 
         //根据关键字图片盖章
         String keyWords = "电子签章";
@@ -27,12 +28,12 @@ public class PdfBoxStampTest {
         float widthScale = 1f;//印章宽度缩放比例
         float heightScale = 1f;//印章高度缩放比例
         boolean compress = true;//参数决定了写入的内容是否应该被压缩。如果设置为true，则PDFBox会尝试压缩内容以减少文件大小；如果设置为false，则内容将以未压缩的形式写入。通常，启用压缩是一个好主意，因为它可以减少生成的PDF文件的大小
-//        stampByKeyWords(pdfPath, stampImgPath, outPdfPath, keyWords, -1, xOffset, yOffset, widthScale, heightScale, compress, true);
+        stampByKeyWords(pdfPath, stampImgPath, outPdfPath, keyWords, -1, xOffset, yOffset, widthScale, heightScale, compress, true);
 
         //根据绝对位置图片盖章
         float x = 49.182f - 40f; // 印章的x坐标
         float y = 724.82f - 90f; // 印章的y坐标（根据页面大小调整）
-        stampByAbsolutePosition(pdfPath, stampImgPath, outPdfPath, 2, x, y, widthScale, heightScale, compress, true);
+//        stampByAbsolutePosition(pdfPath, stampImgPath, outPdfPath, 2, x, y, widthScale, heightScale, compress, true);
     }
 
     /**
