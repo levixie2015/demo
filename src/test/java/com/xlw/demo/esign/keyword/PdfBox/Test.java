c91d344f40d42224b2ffe8b12444710c2ccfa6f2
xieliwei <xieliwei@fertex.cn>
2024-08-23 11:08:23 +0800

    PDFBox库来实现PDF文件的关键字盖章

--- src/test/java/com/xlw/demo/esign/keyword/PdfBox/Test.java
+++ src/test/java/com/xlw/demo/esign/keyword/PdfBox/Test.java
@@ -21,6 +21,23 @@ public class Test {
         float heightScale = 0.5f;//印章高度缩放比例
         boolean compress = true;//参数决定了写入的内容是否应该被压缩。如果设置为true，则PDFBox会尝试压缩内容以减少文件大小；如果设置为false，则内容将以未压缩的形式写入。通常，启用压缩是一个好主意，因为它可以减少生成的PDF文件的大小
 
+        //图片盖章
+        stamp(pdfPath, stampImgPath, keyWords, xOffset, yOffset, widthScale, heightScale, compress);
+    }
+
+    /**
+     * 图片盖章
+     *
+     * @param pdfPath      pdf文档路径
+     * @param stampImgPath 图片印章路径
+     * @param keyWords     关键字
+     * @param xOffset      印章的x坐标偏移量
+     * @param yOffset      印章的y坐标偏移量
+     * @param widthScale   印章宽度缩放比例
+     * @param heightScale  印章高度缩放比例
+     * @param compress     参数决定了写入的内容是否应该被压缩。如果设置为true，则PDFBox会尝试压缩内容以减少文件大小；如果设置为false，则内容将以未压缩的形式写入。通常，启用压缩是一个好主意，因为它可以减少生成的PDF文件的大小
+     */
+    public static void stamp(String pdfPath, String stampImgPath, String keyWords, float xOffset, float yOffset, float widthScale, float heightScale, boolean compress) {
         try (PDDocument doc = PDDocument.load(new File(pdfPath))) {
             PDImageXObject stampImg = PDImageXObject.createFromFile(stampImgPath, doc);
             PDFTextStripperByKeyWord keyWordPosition = new PDFTextStripperByKeyWord(keyWords, pdfPath);
