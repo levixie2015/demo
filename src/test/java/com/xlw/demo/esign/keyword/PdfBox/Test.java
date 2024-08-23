a08ab37ee84677ee13b333f4ce7ef5a620a2fab8
xieliwei <xieliwei@fertex.cn>
2024-08-23 09:47:53 +0800

    PDFBox库来实现PDF文件的关键字盖章

--- src/test/java/com/xlw/demo/esign/keyword/PdfBox/Test.java
+++ src/test/java/com/xlw/demo/esign/keyword/PdfBox/Test.java
@@ -19,6 +19,7 @@ public class Test {
         float yOffset = -90f;//印章的y坐标偏移量
         float widthScale = 0.5f;//印章宽度缩放比例
         float heightScale = 0.5f;//印章高度缩放比例
+        boolean compress = true;//参数决定了写入的内容是否应该被压缩。如果设置为true，则PDFBox会尝试压缩内容以减少文件大小；如果设置为false，则内容将以未压缩的形式写入。通常，启用压缩是一个好主意，因为它可以减少生成的PDF文件的大小
 
         try (PDDocument doc = PDDocument.load(new File(pdfPath))) {
             PDImageXObject stampImg = PDImageXObject.createFromFile(stampImgPath, doc);
@@ -32,7 +33,7 @@ public class Test {
                 PDPage page = doc.getPage((int) position[2] - 1);
                 float x = position[0] + xOffset;
                 float y = position[1] + yOffset;
-                contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true, true);
+                contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, compress, true);
                 contentStream.drawImage(stampImg, x, y, stampImg.getWidth() * widthScale, stampImg.getHeight() * heightScale);
                 contentStream.close();
             }
