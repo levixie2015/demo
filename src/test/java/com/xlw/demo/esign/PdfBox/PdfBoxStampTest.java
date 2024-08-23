cc750d2004129f40f2310bd70e7f13c8e09a1757
xieliwei <xieliwei@fertex.cn>
2024-08-23 17:39:27 +0800

    PDFBox库来实现PDF文件的关键字盖章

--- src/test/java/com/xlw/demo/esign/PdfBox/PdfBoxStampTest.java
+++ src/test/java/com/xlw/demo/esign/PdfBox/PdfBoxStampTest.java
@@ -26,7 +26,7 @@ public class PdfBoxStampTest {
         float widthScale = 1f;//印章宽度缩放比例
         float heightScale = 1f;//印章高度缩放比例
         boolean compress = true;//参数决定了写入的内容是否应该被压缩。如果设置为true，则PDFBox会尝试压缩内容以减少文件大小；如果设置为false，则内容将以未压缩的形式写入。通常，启用压缩是一个好主意，因为它可以减少生成的PDF文件的大小
-        stampByKeyWords(pdfPath, stampImgPath, keyWords, 0, xOffset, yOffset, widthScale, heightScale, compress, true);
+        stampByKeyWords(pdfPath, stampImgPath, keyWords, -1, xOffset, yOffset, widthScale, heightScale, compress, true);
 
         //根据绝对位置图片盖章
         float x = 49.182f - 40f; // 印章的x坐标
@@ -61,6 +61,10 @@ public class PdfBoxStampTest {
                 //若大于索引，则为最后一个
                 if (keyWordIndex >= keyWordPositionList.size() - 1) {
                     keyWordIndex = keyWordPositionList.size() - 1;
+                }
+                //若大于索引，则为最后一个
+                if (keyWordIndex < 0) {
+                    keyWordIndex = 0;
                 }
                 if (i == keyWordIndex) {
                     float[] position = keyWordPositionList.get(i);
