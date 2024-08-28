6096e13e077c6cef703b1f3b00a1b2f52a0db271
xieliwei <xieliwei@fertex.cn>
2024-08-28 11:14:34 +0800

    PDFBox库来实现PDF文件的关键字盖章

--- src/test/java/com/xlw/demo/esign/PdfBox/p12/Pkcs.java
+++ src/test/java/com/xlw/demo/esign/PdfBox/p12/Pkcs.java
@@ -1,5 +1,5 @@
 package com.xlw.demo.esign.PdfBox.p12;
- 
+
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.x500.X500Name;
@@ -10,7 +10,7 @@ import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
- 
+
 import java.io.*;
 import java.math.BigInteger;
 import java.security.*;
@@ -19,43 +19,45 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.*;
- 
+
 /**
+ * <p>Java Keytool：生成数字证书(.cer/.p12文件)的实践指南: https://developer.baidu.com/article/details/3311057</p>
+ *
  * @author bao
  * @date 2024/3/20 16:10
  */
 public class Pkcs {
     private static KeyPair getKey() throws NoSuchAlgorithmException {
         // 密钥对 生成器，RSA算法 生成的  提供者是 BouncyCastle
-        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA",  new BouncyCastleProvider());
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", new BouncyCastleProvider());
         generator.initialize(1024);  // 密钥长度 1024
         // 证书中的密钥 公钥和私钥
         KeyPair keyPair = generator.generateKeyPair();
         return keyPair;
     }
- 
+
     /**
-     * @param password  密码
-     * @param issuerStr 颁发机构信息
-     * @param subjectStr 使用者信息
+     * @param password       密码
+     * @param issuerStr      颁发机构信息
+     * @param subjectStr     使用者信息
      * @param certificateCRL 颁发地址
      * @return
      */
     public static Map<String, byte[]> createCert(String password, String issuerStr, String subjectStr, String certificateCRL) {
- 
+
         Map<String, byte[]> result = new HashMap<String, byte[]>();
         ByteArrayOutputStream out = null;
         try {
             //  生成JKS证书
             //  KeyStore keyStore = KeyStore.getInstance("JKS");
             //  标志生成PKCS12证书
-            KeyStore keyStore = KeyStore.getInstance("PKCS12",  new BouncyCastleProvider());
+            KeyStore keyStore = KeyStore.getInstance("PKCS12", new BouncyCastleProvider());
             keyStore.load(null, null);
             KeyPair keyPair = getKey();
             //  issuer与 subject相同的证书就是CA证书
-            Certificate cert = generateCertificateV3(issuerStr, subjectStr,  keyPair, result, certificateCRL, null);
+            Certificate cert = generateCertificateV3(issuerStr, subjectStr, keyPair, result, certificateCRL, null);
             // cretkey随便写，标识别名
-            keyStore.setKeyEntry("cretkey",  keyPair.getPrivate(),  password.toCharArray(),  new Certificate[] { cert });
+            keyStore.setKeyEntry("cretkey", keyPair.getPrivate(), password.toCharArray(), new Certificate[]{cert});
             out = new ByteArrayOutputStream();
             cert.verify(keyPair.getPublic());
             keyStore.store(out, password.toCharArray());
@@ -74,7 +76,7 @@ public class Pkcs {
         }
         return result;
     }
- 
+
     /**
      * @param issuerStr
      * @param subjectStr
@@ -100,29 +102,29 @@ public class Pkcs {
             // 证书序列号
             BigInteger serial = BigInteger.probablePrime(256, new Random());
             X509v3CertificateBuilder builder = new JcaX509v3CertificateBuilder(
-                    new X500Name(issuerStr), serial, notBefore, notAfter,new X500Name(subjectStr), publicKey);
-            JcaContentSignerBuilder jBuilder = new JcaContentSignerBuilder( "SHA1withRSA");
+                    new X500Name(issuerStr), serial, notBefore, notAfter, new X500Name(subjectStr), publicKey);
+            JcaContentSignerBuilder jBuilder = new JcaContentSignerBuilder("SHA1withRSA");
             SecureRandom secureRandom = new SecureRandom();
             jBuilder.setSecureRandom(secureRandom);
-            ContentSigner singer = jBuilder.setProvider(  new BouncyCastleProvider()).build(privateKey);
+            ContentSigner singer = jBuilder.setProvider(new BouncyCastleProvider()).build(privateKey);
             // 分发点
-            ASN1ObjectIdentifier cRLDistributionPoints = new ASN1ObjectIdentifier( "2.5.29.31");
-            GeneralName generalName = new GeneralName( GeneralName.uniformResourceIdentifier, certificateCRL);
+            ASN1ObjectIdentifier cRLDistributionPoints = new ASN1ObjectIdentifier("2.5.29.31");
+            GeneralName generalName = new GeneralName(GeneralName.uniformResourceIdentifier, certificateCRL);
             GeneralNames seneralNames = new GeneralNames(generalName);
-            DistributionPointName distributionPoint = new DistributionPointName( seneralNames);
+            DistributionPointName distributionPoint = new DistributionPointName(seneralNames);
             DistributionPoint[] points = new DistributionPoint[1];
             points[0] = new DistributionPoint(distributionPoint, null, null);
             CRLDistPoint cRLDistPoint = new CRLDistPoint(points);
             builder.addExtension(cRLDistributionPoints, true, cRLDistPoint);
             // 用途
-            ASN1ObjectIdentifier keyUsage = new ASN1ObjectIdentifier( "2.5.29.15");
+            ASN1ObjectIdentifier keyUsage = new ASN1ObjectIdentifier("2.5.29.15");
             // | KeyUsage.nonRepudiation | KeyUsage.keyCertSign
-            builder.addExtension(keyUsage, true, new KeyUsage( KeyUsage.digitalSignature | KeyUsage.keyEncipherment));
+            builder.addExtension(keyUsage, true, new KeyUsage(KeyUsage.digitalSignature | KeyUsage.keyEncipherment));
             // 基本限制 X509Extension.java
             ASN1ObjectIdentifier basicConstraints = new ASN1ObjectIdentifier("2.5.29.19");
             builder.addExtension(basicConstraints, true, new BasicConstraints(true));
             // privKey:使用自己的私钥进行签名，CA证书
-            if (extensions != null){
+            if (extensions != null) {
                 for (Extension ext : extensions) {
                     builder.addExtension(
                             new ASN1ObjectIdentifier(ext.getOid()),
@@ -132,7 +134,7 @@ public class Pkcs {
             }
             X509CertificateHolder holder = builder.build(singer);
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            bout = new ByteArrayInputStream(holder.toASN1Structure() .getEncoded());
+            bout = new ByteArrayInputStream(holder.toASN1Structure().getEncoded());
             cert = (X509Certificate) cf.generateCertificate(bout);
             byte[] certBuf = holder.getEncoded();
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -158,41 +160,43 @@ public class Pkcs {
         }
         return cert;
     }
- 
+
     public class Extension {
         private String oid;
         private boolean critical;
         private byte[] value;
- 
+
         public String getOid() {
             return oid;
         }
+
         public byte[] getValue() {
             return value;
         }
+
         public boolean isCritical() {
             return critical;
         }
     }
- 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         // CN: 名字与姓氏    OU : 组织单位名称
         // O ：组织名称  L : 城市或区域名称  E : 电子邮件
         // ST: 州或省份名称  C: 单位的两字母国家代码
         String issuerStr = "CN=ibright.com,OU=ibright.com,O=ibright.com,C=CN,E=bnmjstu@ibright.com,L=BeiJin,ST=BeiJin";
         String subjectStr = "CN=ibright.com,OU=ibright.com,O=ibright.com,C=CN,E=bnmjstu@ibright.com,L=BeiJin,ST=BeiJin";
-        String certificateCRL  = "https://www.ibright.com/";
- 
+        String certificateCRL = "https://www.ibright.com/";
+
         //创建证书
         Map<String, byte[]> result = createCert("ibright2014", issuerStr, subjectStr, certificateCRL);
- 
+
         //获取资源文件路径
         String resourcePath = "./src/main/resources/";
- 
+
         FileOutputStream outPutStream = new FileOutputStream(resourcePath + "./ibright.p12"); // ca.jks
         outPutStream.write(result.get("keyStoreData"));
         outPutStream.close();
- 
+
         FileOutputStream fos = new FileOutputStream(new File(resourcePath + "./ibright.cer"));
         fos.write(result.get("certificateData"));
         fos.flush();
