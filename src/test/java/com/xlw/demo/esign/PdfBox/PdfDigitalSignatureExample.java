99e795f21bed61e497571790937727ce83852a5d
xieliwei <xieliwei@fertex.cn>
2024-08-29 18:57:38 +0800

    PDFBox库来实现PDF文件的关键字盖章

--- src/test/java/com/xlw/demo/esign/PdfBox/PdfDigitalSignatureExample.java
+++ src/test/java/com/xlw/demo/esign/PdfBox/PdfDigitalSignatureExample.java
@@ -1,11 +1,12 @@
 package com.xlw.demo.esign.PdfBox;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
+import org.apache.pdfbox.pdmodel.interactive.digitalsignature.SignatureInterface;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class PdfDigitalSignatureExample {
     public static void main(String[] args) throws IOException {
@@ -19,13 +20,15 @@ public class PdfDigitalSignatureExample {
         signature.setName("Signer Name");
         signature.setLocation("Location");
         signature.setReason("Reason for signing");
-
-        // 在文档的最后一个页面上添加签名
-        PDPage lastPage = document.getPage(document.getNumberOfPages() - 1);
-        signature.setByteRange(new int[]{100, 600, 200, 100}); // 签名矩形位置和大小
-
+//        signature.setByteRange(new int[]{100, 600, 200, 100}); // 签名矩形位置和大小
         // 将签名添加到签名字典
-        document.addSignature(signature);
+//        document.addSignature(signature);
+        document.addSignature(signature, new SignatureInterface() {
+            @Override
+            public byte[] sign(InputStream inputStream) throws IOException {
+                return new byte[0];
+            }
+        });
         document.save("/Users/xieliwei/Desktop/电子签章测试/瑞康医药集团河北有限公司PdfBox_签名.pdf");
         document.close();
     }
